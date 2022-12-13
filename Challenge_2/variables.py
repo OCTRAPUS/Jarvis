f815a561a7068a968210dd70dff03dfe697c5378
Octrapus <munjaal.ankur@gmail.com>
2022-12-13 13:38:53 +0530

    Removed the access key

--- Challenge_2/variables.py
+++ Challenge_2/variables.py
@@ -1,5 +1,4 @@
-access_key = "AKIAWX3CBWLFPYEKNU4L"
-secret = "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"
+
 
 
 data_list = ['ami-id', 'ami-launch-index', 'ami-manifest-path', 'block-device-mapping/', 'events/', 'hostname', 'iam/', 'identity-credentials/', 'instance-action', 'instance-id', 'instance-life-cycle', 'instance-type', 'local-hostname', 'local-ipv4', 'mac', 'metrics/', 'network/', 'placement/', 'profile', 'public-hostname', 'public-ipv4', 'public-keys/', 'reservation-id', 'security-groups', 'services/', 'system']
