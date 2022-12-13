7e7342ba555f512ce9e920c97bcc8ff77462305d
Octrapus <munjaal.ankur@gmail.com>
2022-12-13 13:20:34 +0530

    Updated the Exception handling

--- Challenge_3/jarvis.py
+++ Challenge_3/jarvis.py
@@ -1,11 +1,20 @@
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 
 def get_dict_value(str_keys, str_dictionary):
     dictionary = json.loads(str_dictionary)
-    keys = str_keys.split('/')
-    for key in keys:
-        dictionary = get_value(dictionary, key)
+    try:
+        keys = str_keys.split('/')
+        for key in keys:
+            dictionary = get_value(dictionary, key)
+    except KeyError:
+        logging.error("Error with the Key")
+    except Exception as ex:
+        logging.error(f"Hit the Exception {ex}")
+
     print(dictionary)
 
 
